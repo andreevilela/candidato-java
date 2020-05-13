0357ca26f1cd51cfb7180ddb44d5ffc821a00c8a
Andre Vilela <andrevibegoogle@hotmail.com>
2020-05-13 20:24:29 -0300

    Leitura da escolaridade

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,28 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int escolaridade;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós Graduação");
+		System.out.println("Digite uma opção: ");
+		escolaridade = sc.nextInt();
+		
+		sc.close();
+
+	}
+
+}
